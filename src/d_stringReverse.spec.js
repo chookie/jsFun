ba06e11311b9719e52119385d4e14f048afa0ce4
Alison <alison@johnston.name>
2018-06-24 07:32:18 +0100

    progress

--- src/d_stringReverse.spec.js
+++ src/d_stringReverse.spec.js
@@ -1,12 +1,21 @@
-/**
- * Use and array but do it in place in the same array
- */
-const reverseString = (forward) => {
-  if (!forward) return forward;
+const strConcat = (input) => {
+  if (!input) {
+    return input;
+  }
+  let rev = '';
+  for (let i = input.length-1; i >= 0; i--) {
+    rev += input[i];
+  }
+  return rev;
+};
 
-  const array = forward.split('');
-  const len =  array.length;
-  for (let i=0; i < len/2; i++) {
+const strInPlace = (input) => {
+  if (!input) {
+    return input;
+  }
+  const array = input.split('');
+  const len = array.length;
+  for (let i = 0; i < len/2; i++) {
     const temp = array[len - 1 - i];
     array[len - 1 - i] = array[i];
     array[i] = temp;
@@ -14,17 +23,35 @@ const reverseString = (forward) => {
   return array.join('');
 };
 
+const reduce = (input) => {
+  if (!input) {
+    return input;
+  }
+  return input.split('').reduce((acc, val) => val + acc, '');
+};
+
+const each = (input) => {
+  if (!input) return input;
+  let rev = '';
+  input.split('').forEach(c => rev = c + rev);
+  return rev;
+};
+
+const functions = [strConcat, strInPlace, reduce, each];
+
 describe('d: reverse a string', () => {
   const runs = [
-    { description: 'reverse string', input: 'absdef', expected: 'fedsba' },
+    { description: 'reverse string', input: 'abcdef', expected: 'fedcba' },
     { description: 'empty string', input: '', expected: '' },
     { description: 'null string', input: null, expected: null },
     { description: 'single character', input: 'f', expected: 'f' },
     { description: 'mixed case', input: 'AbDgT', expected: 'TgDbA' },
   ];
   runs.forEach(run => {
-    it(`should pass ${run.description}`, () => {
-      expect(reverseString(run.input)).toBe(run.expected);
+    functions.forEach(func => {
+      it(`${func.name} should pass ${run.description}`, () => {
+        expect(func.call(this, run.input)).toBe(run.expected);
+      });
     });
   });
 });
